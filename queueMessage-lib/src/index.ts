d04de9f07a7a6c474143564d701bb53b7e8b0672
mvs24 <Marius.Vasili@fti.edu.al>
2021-08-03 18:17:34 +0200

    Updates

--- queueMessage-lib/src/index.ts
+++ queueMessage-lib/src/index.ts
@@ -1,6 +1,5 @@
-
 export * from "./nats-streaming/models/Publisher";
 export * from "./nats-streaming/models/Listener";
 export * from "./nats-streaming/subjects/subjects";
-export * from "./nats-streaming/events/order-events/OrderCreatedEvent"
-
+export * from "./nats-streaming/events/order-events/OrderCreatedEvent";
+export * from "./nats-streaming/events/email-events/SuccessfulOrderEmailEvent";
